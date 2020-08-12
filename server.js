ae8ab6b15749b4486ef4ca16b8d5ab743f6e8a31
michaeldavidpryor <mdpcaps@gmail.com>
2020-08-12 15:40:31 -0400

    inital commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const routes = require('./routes/handlers');
+
+const PORT = process.env.PORT || 9001;
+const app = express();
+
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is starting at PORT ${PORT}`);
+});
